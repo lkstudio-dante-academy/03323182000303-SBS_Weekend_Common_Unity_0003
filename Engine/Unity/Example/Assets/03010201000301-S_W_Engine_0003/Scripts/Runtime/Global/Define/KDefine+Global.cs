6646becca6e7d7fc3b1d030f19a9f638fbfa70be
lkstudio.dante <lkstudio.dante@gmail.com>
2024-09-07 01:16:15 +0900

    no message

---
+++ Engine/Unity/Example/Assets/03010201000301-S_W_Engine_0003/Scripts/Runtime/Global/Define/KDefine+Global.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * 상수
+ */
+public static partial class KDefine
+{
+	#region 컴파일 상수
+	// 씬 이름
+	public const string G_N_SCENE_EXAMPLE_01 = "E01Example_01 (기초)";
+	public const string G_N_SCENE_EXAMPLE_02 = "E01Example_02 (물리)";
+	#endregion // 컴파일 상수
+}
---
+++ Programming/C#/Example/Example/03010201000301-S_W_Engine_0003/E01/Practice/Classes/Runtime/Practice_01/CP01Practice_01.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example._03010201000301_S_W_Engine_0003.E01.Practice.Classes.Runtime.Practice_01
+{
+	class CP01Practice_01
+	{
+		/** 초기화 */
+		public static void Start(string[] args)
+		{
+			Random oRandom = new Random();
+			int nAnswer = oRandom.Next(1, 101);
+
+			Console.WriteLine("정답 : {0}\n", nAnswer);
+
+			do
+			{
+				Console.Write("숫자 입력 : ");
+				int.TryParse(Console.ReadLine(), out int nNum);
+
+				// 정답 일 경우
+				if(nNum == nAnswer)
+				{
+					Console.WriteLine("정답입니다.");
+					break;
+				}
+				else
+				{
+					string oHint = (nNum < nAnswer) ?
+						"큽니다." : "작습니다.";
+
+					Console.WriteLine("정답은 {0} 보다 {1}\n",
+						nNum, oHint);
+				}
+			} while(true);
+
+			Console.WriteLine("\n게임을 종료합니다.");
+		}
+	}
+}
